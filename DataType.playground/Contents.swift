05297a0b225fc3ee40faa4d0f8d5053a5c91d739
Jun <guswns2614@gmail.com>
2022-05-26 19:24:49 +0900

    Data Type 실습

---
+++ DataType.playground/Contents.swift
@@ -0,0 +1,39 @@
+import UIKit
+
+var isChecked : Bool = false
+isChecked = true
+
+var temperature : Int = -100
+temperature = 100
+
+var cakes : UInt = 100
+    //cakes = -100
+
+var pi : Float = 3.14
+pi = 314
+
+var pi2 : Double = 3.14
+pi2 = 314
+
+var sampleCharacter : Character = "A"
+sampleCharacter = "가"
+
+var sampleString : String = "Hi"
+sampleString = "안녕하세요"
+
+
+var test = 100
+type(of: test)
+
+var testString = "타입추론"
+type(of : testString)
+
+var sampleAny : Any = "any"
+sampleAny = 10000
+sampleAny = 3.14
+
+var sampleInt : Int? = nil
+type(of : sampleInt)
+
+var sampleStr : String? = nil
+type(of : sampleStr)
